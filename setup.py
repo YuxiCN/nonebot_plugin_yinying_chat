b5483a39c2c535cd6866ffd758f02cd1d3fb424f
YuxiCN <79129640+YuxiCN@users.noreply.github.com>
2024-04-18 16:43:07 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nonebot-plugin-yinying-chat',
-    version='1.1.7',
+    version='1.1.8',
     description='A nonebot plugin for yinying-chat',
     author='Yuanluo',
     author_email='3313512421@qq.com',
